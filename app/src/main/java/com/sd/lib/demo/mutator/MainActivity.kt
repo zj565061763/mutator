34b2226ebec983f546c65516566340cc487d22d3
zhengjun <565061763@qq.com>
2022-07-01 23:14:20 +0800

    demo

--- app/src/main/java/com/sd/lib/demo/mutator/MainActivity.kt
+++ app/src/main/java/com/sd/lib/demo/mutator/MainActivity.kt
@@ -6,57 +6,54 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.sd.lib.demo.mutator.databinding.ActivityMainBinding
-import com.sd.lib.mutator.FMutator
+import com.sd.lib.mutator.FScopeMutator
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var binding: ActivityMainBinding
-
-    private val _mutator = FMutator()
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val _mutator by lazy { FScopeMutator(lifecycleScope) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
     override fun onClick(v: View) {
         when (v.id) {
-            R.id.btn_1 -> {
-                lifecycleScope.launch {
-                    try {
-                        mutate(0, "btn_1")
-                    } catch (e: Exception) {
-                        Log.e(TAG, "btn_1 mutate Exception:$e")
-                        e.printStackTrace()
-                        throw e
-                    }
+            R.id.btn_launch -> {
+                _mutator.launch {
+                    start("launch")
                 }
             }
-            R.id.btn_2 -> {
-                lifecycleScope.launch {
-                    mutate(1, "btn_2")
+            R.id.btn_mutate_1 -> {
+                _mutator.launchMutate {
+                    start("mutate_1")
                 }
             }
-            R.id.btn_cancel -> {
+            R.id.btn_mutate_2 -> {
+                _mutator.launchMutate(priority = 1) {
+                    start("mutate_2")
+                }
+            }
+            R.id.btn_cancel_mutate -> {
                 _mutator.cancel()
+            }
+            R.id.btn_cancel_launch -> {
+                _mutator.cancelLaunch()
             }
         }
     }
 
-    private suspend fun mutate(priority: Int, tag: String) {
-        _mutator.mutate(priority) {
-            Log.i(TAG, "$tag delay before")
-            try {
-                delay(5000)
-            } catch (e: Exception) {
-                Log.e(TAG, "$tag delay Exception:$e")
-                e.printStackTrace()
-                throw e
-            }
-            Log.i(TAG, "$tag delay after")
+    private suspend fun start(tag: String) {
+        Log.i(TAG, "$tag delay before")
+        try {
+            delay(5000)
+        } catch (e: Exception) {
+            Log.e(TAG, "$tag delay Exception:$e")
+            e.printStackTrace()
+            throw e
         }
+        Log.i(TAG, "$tag delay after")
     }
 
     companion object {
